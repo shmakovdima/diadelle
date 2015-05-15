8b53b0039e127eae24ceb399d2f8f403bb9e6add
shmakovdima <contact@shmakovdima.ru>
2015-05-15 14:42:41 +0300

    добавляем opencart

---
+++ opencart/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://diadelle.ru/admin/');
+define('HTTP_CATALOG', 'http://diadelle.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://diadelle.ru/admin/');
+define('HTTPS_CATALOG', 'http://diadelle.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/a/aequivph/aequivph.bget.ru/public_html/admin/');
+define('DIR_SYSTEM', '/home/a/aequivph/aequivph.bget.ru/public_html/system/');
+define('DIR_LANGUAGE', '/home/a/aequivph/aequivph.bget.ru/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/a/aequivph/aequivph.bget.ru/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/a/aequivph/aequivph.bget.ru/public_html/system/config/');
+define('DIR_IMAGE', '/home/a/aequivph/aequivph.bget.ru/public_html/image/');
+define('DIR_CACHE', '/home/a/aequivph/aequivph.bget.ru/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/a/aequivph/aequivph.bget.ru/public_html/system/download/');
+define('DIR_UPLOAD', '/home/a/aequivph/aequivph.bget.ru/public_html/system/upload/');
+define('DIR_LOGS', '/home/a/aequivph/aequivph.bget.ru/public_html/system/logs/');
+define('DIR_MODIFICATION', '/home/a/aequivph/aequivph.bget.ru/public_html/system/modification/');
+define('DIR_CATALOG', '/home/a/aequivph/aequivph.bget.ru/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'aequivph_diadell');
+define('DB_PASSWORD', 'as210100');
+define('DB_DATABASE', 'aequivph_diadell');
+define('DB_PREFIX', 'oc_');
---
+++ opencart/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://diadelle.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://diadelle.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/a/aequivph/aequivph.bget.ru/public_html/catalog/');
+define('DIR_SYSTEM', '/home/a/aequivph/aequivph.bget.ru/public_html/system/');
+define('DIR_LANGUAGE', '/home/a/aequivph/aequivph.bget.ru/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/a/aequivph/aequivph.bget.ru/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/a/aequivph/aequivph.bget.ru/public_html/system/config/');
+define('DIR_IMAGE', '/home/a/aequivph/aequivph.bget.ru/public_html/image/');
+define('DIR_CACHE', '/home/a/aequivph/aequivph.bget.ru/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/a/aequivph/aequivph.bget.ru/public_html/system/download/');
+define('DIR_UPLOAD', '/home/a/aequivph/aequivph.bget.ru/public_html/system/upload/');
+define('DIR_MODIFICATION', '/home/a/aequivph/aequivph.bget.ru/public_html/system/modification/');
+define('DIR_LOGS', '/home/a/aequivph/aequivph.bget.ru/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'aequivph_diadell');
+define('DB_PASSWORD', 'as210100');
+define('DB_DATABASE', 'aequivph_diadell');
+define('DB_PREFIX', 'oc_');
